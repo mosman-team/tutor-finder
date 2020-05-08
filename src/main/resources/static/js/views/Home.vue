b845b32e27cd0bb6b48a91df2d4dd7808e3e8e6c
omir <omir.musradin.99@gmail.com>
2020-05-08 21:49:40 +0600

    half home page

--- src/main/resources/static/js/views/Home.vue
+++ src/main/resources/static/js/views/Home.vue
@@ -1,13 +1,33 @@
 <template>
-    <div class="container">
-        <header class="jumbotron">
-            <h3>{{content}}</h3>
-        </header>
-    </div>
+    <!--<v-parallax
+            dark
+            src="http://localhost:8080/static/img/homeImg.jpg">
+        <v-row
+                align="center"
+                justify="center">
+            <v-col class="text-center" cols="12">
+                <h1 class="display-1 font-weight-thin mb-4">Vuetify.js</h1>
+                <h4 class="subheading">Build your application today!</h4>
+            </v-col>
+        </v-row>
+    </v-parallax>
+    -->
+
+
+    <v-img class="mx-auto" width="1000" src="http://localhost:8080/static/img/homeImg.jpg">
+        <v-row
+                align="center"
+                justify="center">
+            <v-col class="text-center" cols="12">
+                <h1 class="display-1 font-weight-thin mb-4">Vuetify.js</h1>
+                <h4 class="subheading">Build your application today!</h4>
+            </v-col>
+        </v-row>
+    </v-img>
+
 </template>
 
 <script>
-    import UserService from '../services/user.service';
 
     export default {
         name: 'Home',
@@ -17,17 +37,7 @@
             };
         },
         mounted() {
-            /*UserService.getPublicContent().then(
-                response => {
-                    this.content = response.data;
-                },
-                error => {
-                    this.content =
-                        (error.response && error.response.data) ||
-                        error.message ||
-                        error.toString();
-                }
-            );*/
+
         }
     };
 </script>
